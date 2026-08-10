c7bd72e1818558defc89e10d3ce365dfe567cdc6
Soren Cooper <Clumsycupcake14@icloud.com>
2026-08-10 12:45:36 -0700

    Add config

---
+++ config.js
@@ -0,0 +1,12 @@
+window.CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyAD-G8fAlvuNmTYnjyt1UtLa_wXhpHKHuM",
+    authDomain: "cvhs-compost.firebaseapp.com",
+    projectId: "cvhs-compost",
+    storageBucket: "cvhs-compost.firebasestorage.app",
+    messagingSenderId: "966623231956",
+    appId: "1:966623231956:web:67dc3f5f4eb5b85f0aa041"
+  },
+  adminPhone: "8182793907",
+  lbsPerContribution: 0.5
+};
